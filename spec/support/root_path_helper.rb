0d3fe404220d4df73e7abeb4b01492b5741aede3
zm1994 <volniy.ptaha@gmail.com>
2017-01-20 11:45:20 +0200

    last

--- spec/support/root_path_helper.rb
+++ spec/support/root_path_helper.rb
@@ -1,4 +1,4 @@
 $root_path_avia = 'https://tripway.dev/'
-$root_path_railway = 'https://tripway.dev/railway'
+$root_path_railway = 'https://tripway.dev/railway/'
 $root_path_mobile_avia = $root_path_avia + '?mobile=1'
 $array_web_mobile_root_path = [$root_path_avia, $root_path_mobile_avia]
